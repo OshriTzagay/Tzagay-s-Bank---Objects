0bd2082c4642c18d097c7a2403b4e3564004aa49
OshriTzagay <oshritzagay@gmail.com>
2021-08-15 18:26:50 +0300

    update-finised

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const tzagaysBank = [
     phone: "0526367723",
   },
   {
-    id: 888888888,
+    id: 1,
     firstName: "avi",
     lastName: "admso",
     email: "aviadmso@gmail.com",
@@ -16,7 +16,7 @@ const tzagaysBank = [
     phone: "67676767676",
   },
   {
-    id: 777777777,
+    id: 2,
     firstName: "amir",
     lastName: "mangisto",
     email: "amiros@gmail.com",
@@ -24,7 +24,7 @@ const tzagaysBank = [
     phone: "45454545454",
   },
   {
-    id: 666666666,
+    id: 3,
     firstName: "eliyau",
     lastName: "belay",
     email: "thebelay@gmail.com",
@@ -32,7 +32,7 @@ const tzagaysBank = [
     phone: "2323232323",
   },
   {
-    id: 555555555,
+    id: 4,
     firstName: "yoni",
     lastName: "tagania",
     email: "ys@gmail.com",
@@ -42,19 +42,87 @@ const tzagaysBank = [
 ];
 
 // console.log(tzagaysBank);
-function addClient (newClient){
-tzagaysBank.push(newClient);
-return tzagaysBank;
 
-
+////////////////////////////////////////
+function addClient(newClient) {
+  tzagaysBank.push(newClient);
+  return tzagaysBank;
 }
-addClient ({
-    id: 87878787,
-    firstName: "Jacob",
-    lastName: "Shiber",
-    email: "JShiber@gmail.com",
-    balance: 2220,
-    phone: "666666667",
+addClient({
+  id: 5,
+  firstName: "Jacob",
+  lastName: "Shiber",
+  email: "JShiber@gmail.com",
+  balance: 2220,
+  phone: "666666667",
+});
 
-})
-console.log(tzagaysBank);
+// console.log(tzagaysBank);
+//////////////////////////////////////////
+
+//////////////////////////////////////////
+
+function getClient(id) {
+  for (let i = 0; i < tzagaysBank.length; i++) {
+    if (tzagaysBank[i].id == id) {
+      return tzagaysBank[i];
+    }
+  }
+}
+
+// console.log(getClient(5));
+
+//////////////////////////////////////////
+
+////////////////////////////////////////////
+
+function deleteClient(id) {
+  for (let i = 0; i < tzagaysBank.length; i++) {
+    if (tzagaysBank[i].id == id) {
+      tzagaysBank.splice([i], 1);
+
+      return tzagaysBank;
+    }
+  }
+}
+// console.log(deleteClient(2));
+
+////////////////////////////////////////////
+
+////////////////////////////////////////////
+
+function withdrawMoney(id, amount) {
+  for (let i = 0; i < tzagaysBank.length; i++) {
+    if (tzagaysBank[i].id == id) {
+      tzagaysBank[i].balance = tzagaysBank[i].balance - amount;
+      return tzagaysBank;
+    }
+  }
+}
+// console.log(withdrawMoney(316164144, 500000));
+
+/////////////////////////////////////////////
+
+function depositMoney(id, amount) {
+  for (let i = 0; i < tzagaysBank.length; i++) {
+    if (tzagaysBank[i].id == id) {
+      tzagaysBank[i].balance = tzagaysBank[i].balance + amount;
+      return tzagaysBank;
+    }
+  }
+}
+// console.log(depositMoney(2,2000000));
+
+/////////////////////////////////////////////
+
+function BlockedClients (id){
+
+    let blacklist = [];
+    if (tzagaysBank.id == id){
+
+        blacklist.push([i],1)
+        return blacklist
+    }
+}
+console.log(BlockedClients(2));
+
